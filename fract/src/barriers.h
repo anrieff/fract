390fed378e7656f7618dd860541dd795251bedd8
anrieff1 <anrieff1@4b73aca3-9014-0410-b8ed-9bdd1c1cebd0>
2006-06-18 16:19:56 +0000

    nomsg
    git-svn-id: https://svn.code.sf.net/p/fract/code@39 4b73aca3-9014-0410-b8ed-9bdd1c1cebd0

---
+++ fract/src/barriers.h
@@ -0,0 +1,24 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Veselin Georgiev                                *
+ *   anrieff@mailg.com (convert to gmail)                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+ 
+/*
+	This file contains enumerations for the Barrier class ids
+*/
+#ifndef __BARRIERS_H__
+#define __BARRIERS_H__
+
+enum {
+	VOXEL_LIGHT_INNER2,
+	VOXEL_LIGHT_OUTER,
+	VOXEL_BARRIER1,
+	VOXEL_BARRIER2,
+};
+
+#endif
