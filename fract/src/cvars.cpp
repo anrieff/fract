b3189e1373a37661cccc706776d5dae5ec3d2a04
anrieff1 <anrieff1@4b73aca3-9014-0410-b8ed-9bdd1c1cebd0>
2006-09-11 16:16:21 +0000

    Adding CVar support - added the first few cvars
    git-svn-id: https://svn.code.sf.net/p/fract/code@186 4b73aca3-9014-0410-b8ed-9bdd1c1cebd0

---
+++ fract/src/cvars.cpp
@@ -0,0 +1,15 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by Veselin Georgiev                                *
+ *   anrieff@mailg.com (convert to gmail)                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#define STORAGE
+
+namespace CVars {
+	#include "cvarlist.h"
+};
